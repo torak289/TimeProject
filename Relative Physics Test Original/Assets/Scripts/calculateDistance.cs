630f67d23f63a50c6a1faa1cf7c4d1b2645d584d
torak289 <torak289@gmail.com>
2018-02-04 15:33:32 +0000

    Update
    Added Spacial Speed Change to Orbit.

--- Relative Physics Test Original/Assets/Scripts/calculateDistance.cs
+++ Relative Physics Test Original/Assets/Scripts/calculateDistance.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
 
 public class calculateDistance : MonoBehaviour {
 	bool isTracking;
-	Vector3 distance;
+	private Vector3 distance;
 	public Camera arCamera;
 	// Update is called once per frame
     void FixedUpdate () {
 		distance = arCamera.transform.position - this.transform.localPosition;
-		Debug.Log ("Distance from camera: " + distance.magnitude);
+    }
+
+    public float getDistance() {
+        return distance.magnitude;
     }
 }
--- Relative Physics Test Original/Assets/Scripts/rotatePlanet.cs
+++ Relative Physics Test Original/Assets/Scripts/rotatePlanet.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class rotatePlanet : MonoBehaviour {
 	public GameObject point;
-	public float rotationSpeed;
+	private float rotationSpeed = 1;
+    private Vector3 dist;
+    GameObject imageTarget;
 	// Use this for initialization
 	void Start () {
+        imageTarget = GameObject.Find("ImageTarget");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        calculateDistance calD = imageTarget.GetComponent<calculateDistance>();
+        Debug.Log("Distance from camera: " + calD.getDistance());
+        rotationSpeed = 1 * (calD.getDistance()/10);
+        //Debug.Log("Rotation Speed: " + rotationSpeed);
 		this.transform.RotateAround (point.transform.position, Vector3.up, 10 * Time.deltaTime * rotationSpeed);
 	}
 }
